f9e6f6fffb23996742cf5b71871285cf6b53d919
Thiago Lagden <lagden@gmail.com>
2020-12-21 02:37:05 -0300

    coverage 100%

--- test/index.js
+++ test/index.js
@@ -8,13 +8,20 @@ test('singleton', async t => {
 	const redis = singleton.connect('127.0.0.1:6379')
 	await redis.set('a', 'xxx')
 
-	const redis2 = singleton.connect('127.0.0.1:6379')
+	const redis2 = singleton.connect()
 	const result = await redis2.get('a')
 	t.is(result, 'xxx')
 	t.true(redis === redis2)
 })
 
-test.cb('connect', t => {
+test('connect', async t => {
+	const redis = connect()
+	await redis.set('b', 'yyy')
+	const result = await redis.get('b')
+	t.is(result, 'yyy')
+})
+
+test.cb('cluster', t => {
 	const redis = connect('127.0.0.1,127.0.0.1:6379', {
 		clusterRetryStrategy: () => false
 	})
